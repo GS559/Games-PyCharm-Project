0b7adb39ff903d54db9d1a71e978a483b69a36aa
GS559 <martyc_2006@outlook.fr>
2023-01-26 20:24:45 +0100

    Commit
    -> Changed games code to have a common layout

--- games/games.py
+++ games/games.py
@@ -1,22 +1,54 @@
 import os
-from typing import List
-
+import pygame
 from jproperties import Properties
 
 
 class Games:
 
     @classmethod
-    def list(cls, lang) -> dict:
+    def list(cls, lang: Properties) -> dict:
         """
         returns a dictionary of the form:
         {game_id: game_display_name}
         """
-        l = [str(i).split("\'")[1] for i in list(os.scandir('games'))]
-        l = [i for i in l if not (i.endswith('.py') or i.startswith('__'))]
+        game_list = [str(i).split("\'")[1] for i in list(os.scandir('games'))]
+        game_list = [i for i in game_list if not (i.endswith('.py') or i.startswith('__'))]
 
         d = {}
-        for i in l:
+        for i in game_list:
             d[i] = lang[f'{i}.display_name'].data
 
         return d
+
+    # Game specific methods
+
+    @classmethod
+    def setup_pygame_window(cls, config: Properties, colors: dict, game_name: str) -> pygame.Surface:
+        window = pygame.display.set_mode((800, 500))
+        pygame.display.set_caption(game_name)
+        window.fill(colors['background'])
+
+        pygame.draw.rect(window, colors['top_bar'], pygame.Rect(0, 0, 800, 60))  # Adds the top bar
+        pygame.draw.rect(window, colors['button'], pygame.Rect(15, 10, 100, 40))  # Adds the start button
+        # Adds the start button text
+        start_text = cls.load_fonts(config=config)[0].render('Start', True, colors['black'])
+        start_text_rect = start_text.get_rect()
+        start_text_rect.center = (65, 30)
+
+        window.blit(start_text, start_text_rect)
+        return window
+
+    @classmethod
+    def load_fonts(cls, config: Properties) -> tuple[pygame.font.Font, pygame.font.Font]:
+        normal_font = pygame.font.SysFont(config['font_family'].data, 35)
+        big_font = pygame.font.SysFont(config['font_family'].data, 75)
+        return normal_font, big_font
+
+    @classmethod
+    def top_bar_clear(cls, window: pygame.Surface, colors: dict) -> None:
+        pygame.draw.rect(window, colors['top_bar'], pygame.Rect(150, 0, 650, 60))
+
+    @classmethod
+    def top_bar_write(cls, window: pygame.Surface, config: Properties, text: str, color: tuple) -> None:
+        text = cls.load_fonts(config=config)[0].render(text, True, color)
+        window.blit(text, (150, 10))
--- games/noughts_and_crosses/ui.py
+++ games/noughts_and_crosses/ui.py
@@ -1,10 +1,14 @@
 import pygame
 from jproperties import Properties
 
-from logic import Game
+from games.noughts_and_crosses.logic import Game
+from games.games import Games as GameMethods
+
+game = None
 
 
 def run(config: Properties, lang: Properties) -> None:
+    global game
     pygame.init()
 
     COLORS = {
@@ -17,36 +21,27 @@ def run(config: Properties, lang: Properties) -> None:
             [int(i) for i in config['noughts_and_crosses.player_2_color'].data.split(',')],
         'black': (0, 0, 0)
     }
-    FONT = pygame.font.SysFont(config['font_family'].data, 35)
-    BIG_FONT = pygame.font.SysFont(config['font_family'].data, 75)
+    FONT, BIG_FONT = GameMethods.load_fonts(config=config)
     cells = [
         [(240, 110), (350, 110), (460, 110)],
         [(240, 220), (350, 220), (460, 220)],
         [(240, 330), (350, 330), (460, 330)]
     ]
 
-    window = pygame.display.set_mode((800, 500))
-    pygame.display.set_caption("Noughts and Crosses")
-    window.fill(COLORS['background'])
+    window = GameMethods.setup_pygame_window(config=config, colors=COLORS,
+                                             game_name=lang['noughts_and_crosses.display_name'].data)
 
-    def write(text: str, color_key: tuple) -> None:
-        pygame.draw.rect(window, COLORS['top_bar'], pygame.Rect(150, 0, 650, 60))
-        text = FONT.render(text, True, COLORS[color_key])
-        window.blit(text, (150, 10))
+    def write(text: str, color_key: str) -> None:
+        GameMethods.top_bar_clear(window=window, colors=COLORS)
+        GameMethods.top_bar_write(window=window, config=config, text=text, color=COLORS[color_key])
 
     def clear_board() -> None:
         for i in range(len(cells)):
-            for j in range(len(cells[i])):
-                pygame.draw.rect(window, COLORS['background'], pygame.Rect(cells[i][j][0], cells[i][j][1], 100, 100))
+            for v in range(len(cells[i])):
+                pygame.draw.rect(window, COLORS['background'], pygame.Rect(cells[i][v][0], cells[i][v][1], 100, 100))
 
-    pygame.draw.rect(window, COLORS['top_bar'], pygame.Rect(0, 0, 800, 60))  # Adds the top bar
-    pygame.draw.rect(window, COLORS['button'], pygame.Rect(15, 10, 100, 40))  # Adds the start button
-    start_text = FONT.render('Start', True, COLORS['black'])  # Adds the start button text
-    start_text_rect = start_text.get_rect()  # |
-    start_text_rect.center = (65, 30)  # |
-    window.blit(start_text, start_text_rect)  # |
     for i in [(340, 110, 10, 320), (450, 110, 10, 320), (240, 210, 320, 10), (240, 320, 320, 10)]:  # Adds the grid
-        pygame.draw.rect(window, COLORS['top_bar'], pygame.Rect(*i))  # |
+        pygame.draw.rect(window, COLORS['top_bar'], pygame.Rect(*i))
 
     isTheGameFinished = True
     running = True
